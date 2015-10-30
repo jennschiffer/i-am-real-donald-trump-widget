c9e4bb03eb7a426a6c096a3df86f86f6c7e86700
Jenn Schiffer <jenn@dotbiz.info>
2015-10-30 17:54:56 -0400

    base of widget, prob doesn't work but lol oh well

---
+++ i-am-real-donald-trump-widget.php
@@ -0,0 +1,76 @@
+<?php
+/*
+Plugin Name: I Am Real Donald Trump Widget
+Plugin URI: http://github.com/jennschiffer/i-am-real-donald-trump-widget
+Description: A widget that allows you to be Donald Trump.
+Version: 1.0
+Author: Jenn Schiffer
+Author URI: http://jennmoney.biz
+
+Copyright 2015  Jenn Schiffer  (http://jennmoney.biz)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+add_action('widgets_init', 'featured_comment_widget_init', 1);
+function featured_comment_widget_init() {
+  register_widget('Featured_Comment_Widget');
+  load_plugin_textdomain('i-am-real-donald-trump-widget', false, basename( dirname( __FILE__ ) ) . '/languages' );
+}
+
+add_action('wp_head', 'i_am_real_donald_trump_widget_css');
+function i_am_real_donald_trump_widget_css() {
+  echo '<style type="text/css"></style>';
+}
+
+class I_Am_Real_Donald_Trump_Widget extends WP_Widget {
+
+  public function __construct() {
+    $widget_ops = array('classname' => 'i_am_real_donald_trump_widget', 'description' => __('Enter your twitter handle and become Real Donald Trump','i-am-real-donald-trump-widget'));
+    parent::__construct('i-am-real-donald-trump-widget', __('I Am Real Donald Trump'), $widget_ops);
+  }
+
+  public function widget($args, $instance) {
+    extract($args);
+    $twitterHandle = apply_filters( 'widget_twitterHandle', empty($instance['twitterHandle']) ? '' : $instance['twitterHandle'], $instance, $this->id_base);
+    
+    // no title right now
+    echo $before_widget . $before_title  . $after_title; 
+ 
+    // for each item in comment array, display tweet
+    if ($twitterHandle) {
+      echo '<div class="i-am-real-donald-trump-twitter-handle">' . $twitterHandle . '</div>'; 
+    }
+
+    echo $after_widget;
+  }
+
+  public function form( $instance ) {
+    $instance = wp_parse_args( (array) $instance, array( 'twitterHandle' => '') );
+    $twitterHandle = strip_tags($instance['twitterHandle']);
+
+    echo '<p>' .
+          '<label for="' . $this->get_field_id('twitterHandle') . '">' . _e('Your Twitter Handle','i-am-real-donald-trump-widget') . ': ' . 
+          '<input class="widefat" id="' . $this->get_field_id('twitterHandle') .'" name="' . $this->get_field_name('twitterHandle') .'" type="text" value="' . esc_attr($twitterHandle) . '" /></label>' . 
+         '</p>';
+  }
+  
+  public function update( $new_instance, $old_instance ) {
+    $instance = $old_instance;
+    $instance['twitterHandle'] = strip_tags($new_instance['twitterHandle']);
+    return $instance;
+  }
+  
+}
---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== Plugin Name ===
+Contributors: jennschiffer
+Plugin Name: I Am Real Donald Trump Widget
+Plugin URI: http://github.com/jennschiffer/i-am-real-donald-trump-widget/
+Author: Jenn Schiffer
+Author URI: http://jennmoney.biz/
+Tags: hot, new, widgets, hot new widgets, donald trump, real donald trump, twitter, tweets, hot new tweets
+Tested up to: 4.3
+Stable tag: 1.0
+
+== Description ==
+
+The I Am Real Donald Trump Widget gives you the ability to be the Real Donald Trump, or at least make it look like you tweet like him.
+
+Installing this plugin adds the I Am Real Donald Trump Widget to your list of available widgets in the 'Widgets' submenu (which you can find under 'Appearance' in your WordPress dashboard). Adding your twitter handle to the widget form will make a random Donald tweet look like it came from you. This is super important and will change your life and possibly greatly increase your S.E.O.™
+
+== Changelog ==
+
+1.0 Welcome to the World Wide Web™ my sweet hot new widget!
+
+== Installation ==
+
+1. Upload `i-am-real-donald-trump-widget.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Add the widget through the 'Widgets' submenu under 'Appearance' in WordPress
+4. Add your twitter handle in the widget and save.
+
+== Widget CSS ==
+
+The following are the classes and explanations of them for those of you who want to style your widget in the custom.css file:
+
+== Screenshots ==
+
+1. The I Am Real Donald Trump Widget form.
+2. The I Am Real Donald Trump Widget as shown on an example blog.
